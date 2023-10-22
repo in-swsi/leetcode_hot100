4b88f84dbc824b7d342c96d4a9be1345bad12161
inswsi <inswsi@inswsi.com>
2023-10-22 15:39:50 +0800

    初始化项目

---
+++ cmake-build-debug/CMakeFiles/leetcode_hot100.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_hot100.dir/main.cpp.obj"
+  "CMakeFiles/leetcode_hot100.dir/main.cpp.obj.d"
+  "leetcode_hot100.exe"
+  "leetcode_hot100.exe.manifest"
+  "leetcode_hot100.pdb"
+  "libleetcode_hot100.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode_hot100.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/leetcode_hot100.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for leetcode_hot100.
+# This may be replaced when dependencies are built.
